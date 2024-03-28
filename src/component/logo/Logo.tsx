38c0f1f28ba532688d54ec3b2d6920a46d9a62a9
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-03-28 22:14:17 +0300

    Header

--- src/component/logo/Logo.tsx
+++ src/component/logo/Logo.tsx
@@ -4,7 +4,7 @@ import {Icon} from '../icon/icon';
 export const Logo = () => {
     return (
         <a href=''>
-            <Icon iconId={'ts'}/>
+            <Icon iconId={'logo'} width="40" height="37" viewBox="0 0 40 37" />
         </a>
     );
 };
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,20 +3,32 @@ import styled from 'styled-components';
 import {Menu} from '../../component/menu/Menu';
 import {Logo} from '../../component/logo/Logo';
 import {Projects} from '../sectoins/projects/Projects';
+import {Container} from '../../component/Container';
+import {FlexWrapper} from '../../component/FlexWrapper';
+import {Theme} from '../../styles/Theme';
 
 // rsc
-const headerItems = ["Home", "Projects", "About", "Contacts"]
+const headerItems = ['Home', 'Projects', 'About', 'Contacts']
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={headerItems}/>
+            <Container>
+                <FlexWrapper justifyContent={'space-between'} alignItems={'center'}>
+                    <Logo/>
+                    <Menu menuItems={headerItems}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
-  display: flex;
-  justify-content: space-between;
+  background-color: ${Theme.colors.primaryBg};
+  min-height: 96px;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 999;
 `
 
