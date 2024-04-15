b5f4e186888bd70cb072da729b6e6a5f7b0104b7
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-04-15 21:30:17 +0300

    scroll

--- src/layout/header/headerMenu/menu/Menu.tsx
+++ src/layout/header/headerMenu/menu/Menu.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {S} from '../HeaderMenu_styles'
 
-export const Menu: React.FC<{menuItems: Array<{title: string, href: string}>}> = (props: { menuItems: Array<{title: string, href: string}> }) => {
+export const Menu: React.FC<{ menuItems: Array<{ title: string, href: string }> }> = (props: {
+    menuItems: Array<{ title: string, href: string }>
+}) => {
     return (
         <ul role={'menu'}>
             {props.menuItems.map((item, index) => {
                 return <S.MenuList key={index} role={'menuItem'}>
-                    <S.NavLink to={item.href}
+                    <S.NavLink activeClass="active"
+                               to={item.href}
+                               spy={true}
                                smooth={true}
+                               offset={15}
                     >
                         {item.title}
                         <S.Mask>
